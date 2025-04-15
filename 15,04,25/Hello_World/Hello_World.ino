eddfb48d25e5f5bdd63a4c21dc4a7854099b8204
tugbaaslans <tugbaaslans09@gmail.com>
2025-04-15 14:43:05 +0300

    Add files via upload

---
+++ 15,04,25/Hello_World/Hello_World.ino
@@ -0,0 +1,58 @@
+/*
+    LCD_I2C - Arduino library to control a 16x2 LCD via an I2C adapter based on PCF8574
+
+    Copyright(C) 2020 Blackhack <davidaristi.0504@gmail.com>
+
+    This program is free software : you can redistribute it and /or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see < https://www.gnu.org/licenses/>.
+*/
+
+#include <LCD_I2C.h>
+
+LCD_I2C lcd(0x27, 16, 2); // Default address of most PCF8574 modules, change according
+
+void setup()
+{
+    lcd.begin(); // If you are using more I2C devices using the Wire library use lcd.begin(false)
+                 // this stop the library(LCD_I2C) from calling Wire.begin()
+    // Note: On ESP32 you would rather use:
+    //
+    //   lcd.begin(4,5);
+    //
+    // which maps 'Wire' sda to pin 4 and scl to pin 5 in this example.
+
+    lcd.backlight();
+}
+
+void loop()
+{
+    
+    lcd.setCursor(0,0); // Or setting the cursor in the desired position.
+    lcd.print("TUGBA ");
+    lcd.setCursor(0,1); // Or setting the cursor in the desired position.
+    lcd.print(" ASLAN");
+    delay(500);
+
+    // Flashing the backlight
+    for (int i = 0; i < 5; ++i)
+    {
+        lcd.backlight();
+        delay(50);
+        lcd.noBacklight();
+        delay(50);
+    }
+
+    lcd.backlight();
+    lcd.clear();
+    delay(500);
+}
